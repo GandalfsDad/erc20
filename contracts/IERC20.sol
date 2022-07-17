6b4045399a85a7aa269f2c5a81ecffc6d1bd2e2d
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-07-17 18:19:08 +1000

    Correct IERC20

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -1,28 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract IERC20 {
+abstract contract IERC20 {
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value)
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value)
-    
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    function name() public view returns (string) {}
 
-    function symbol() public view returns (string) {}
+    function name() public virtual view returns (string memory);
 
-    function decimals() public view returns (uint8) {}
+    function symbol() public virtual view returns (string memory);
 
-    function totalSupply() public view returns (uint256) {}
+    function decimals() public virtual view returns (uint8);
 
-    function balanceOf(address _owner) public view returns (uint256 balance) {}
+    function totalSupply() public virtual view returns (uint256);
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {}
+    function balanceOf(address _owner) public virtual view returns (uint256 balance);
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {}
+    function transfer(address _to, uint256 _value) public virtual returns (bool success);
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {}
+    function transferFrom(address _from, address _to, uint256 _value) public virtual returns (bool success);
 
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining)
+    function approve(address _spender, uint256 _value) public virtual returns (bool success);
+
+    function allowance(address _owner, address _spender) public virtual view returns (uint256 remaining);
 
 }
